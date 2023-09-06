808af339faa11ed2b87c5d4f2e5bdbd6d39e3f7e
caicai <1972382086@qq.com>
2023-09-06 09:51:12 +0800

    second commit

--- src/main/java/com/powernode/gitTest.java
+++ src/main/java/com/powernode/gitTest.java
@@ -3,7 +3,8 @@ package com.powernode;
 public class gitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 
 }
